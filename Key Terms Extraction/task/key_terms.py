b1558f63985c064d1a889f25fca7c2e77ba6a681
supermads <madisonsfahey@gmail.com>
2021-08-26 21:05:35 -0700

    Stage 2 of Project

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,6 +1,8 @@
-import nltk
+from nltk import word_tokenize, WordNetLemmatizer
+from nltk.corpus import stopwords
 from lxml import etree
 from collections import Counter
+from string import punctuation
 
 
 def main():
@@ -11,10 +13,19 @@ def main():
             # Print article header
             print(article[0].text + ":")
 
-            # Create freq. dict. for tokens in the article text and find the 5 most common tokens
-            tokens = nltk.word_tokenize(article[1].text.lower())
-            tokens.sort(reverse=True)
-            common_words = Counter(tokens).most_common(5)
+            # Tokenize article text, then lemmatize words and remove stop-words and punctuation
+            tokens = word_tokenize(article[1].text.lower())
+
+            lemmatizer = WordNetLemmatizer()
+            words = []
+            for token in tokens:
+                word = lemmatizer.lemmatize(token)
+                if word not in stopwords.words('english') and word not in punctuation:
+                    words.append(word)
+
+            # Find and print five most common words in article text
+            words.sort(reverse=True)
+            common_words = Counter(words).most_common(5)
 
             print(" ".join([word[0] for word in common_words]) + "\n")
 
