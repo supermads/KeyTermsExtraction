69486fe8905859d3d1d68af0ec475dd551c5111b
supermads <madisonsfahey@gmail.com>
2021-09-01 11:11:26 -0700

    Stage 4 of Project- Finds 5 of the most common words in each of the articles, but doens't pass all of the tests

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,8 +1,8 @@
 from nltk import word_tokenize, WordNetLemmatizer, pos_tag
 from nltk.corpus import stopwords
 from lxml import etree
-from collections import Counter
 from string import punctuation
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def main():
@@ -21,15 +21,25 @@ def main():
             for token in tokens:
                 word = lemmatizer.lemmatize(token)
                 if word not in stopwords.words('english') and word not in punctuation:
-                    # Only include words that are nouns
+                    # Only include nouns
                     if pos_tag([word])[0][1] == "NN":
                         words.append(word)
 
-            # Find and print five most common words in article text
+            # Find and print five most common words in article text using TF-IDF metric for each word in all articles
             words.sort(reverse=True)
-            common_words = Counter(words).most_common(5)
+            vectorizer = TfidfVectorizer()
+            tfidf_matrix = vectorizer.fit_transform([" ".join(words)])
+            terms = vectorizer.get_feature_names()
 
-            print(" ".join([word[0] for word in common_words]) + "\n")
+            ratings = tfidf_matrix.toarray()[0]
+
+            common_word_indices = (-ratings).argsort()[:5]
+
+            common_words = []
+            for index in common_word_indices:
+                common_words.append(terms[index])
+
+            print(" ".join([word for word in common_words]))
 
 
 main()
