b062103b2f904b17a001e983343efa2eb08bdccc
supermads <madisonsfahey@gmail.com>
2021-09-01 13:56:00 -0700

    Refactored Stage 4 of Project

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -22,34 +22,39 @@ def process_article(article):
     return " ".join(words)
 
 
+def find_common_words(vectorizer, header, article):
+    vector = vectorizer.transform([article])
+    vector_array = flip(vector.toarray())[-1]
+
+    feature_names = vectorizer.get_feature_names()
+    feature_names.reverse()
+
+    common_words = []
+    while len(common_words) < 5:
+        max_index = where(vector_array == max(vector_array))[0][0]
+        vector_array[max_index] = 0
+        common_words.append(feature_names[max_index])
+
+    print(header)
+    print(" ".join(common_words))
+
+
 def main():
     root = etree.parse("news.xml").getroot()
     processed_corpus = []
     article_headers = []
+
     for corpus in root:
         for article in corpus:
             processed_corpus.append(process_article(article))
             article_headers.append(article[0].text + ":")
 
-    # Find and print five most common words in article text using TfidfVectorizer
+    # Find and print five most common words in article text using TfidfVectorizer on all articles
     vectorizer = TfidfVectorizer()
     vectorizer.fit(processed_corpus)
 
-    for i in range(len(processed_corpus)):
-        vector = vectorizer.transform([processed_corpus[i]])
-        vector_array = flip(vector.toarray())[-1]
-
-        feature_names = vectorizer.get_feature_names()
-        feature_names.reverse()
-
-        common_words = []
-        while len(common_words) < 5:
-            max_index = where(vector_array == max(vector_array))[0][0]
-            vector_array[max_index] = 0
-            common_words.append(feature_names[max_index])
-
-        print(article_headers[i])
-        print(" ".join(common_words))
+    for header, article in zip(article_headers, processed_corpus):
+        find_common_words(vectorizer, header, article)
 
 
 main()
